acf8f2b5f6687ade5f093b0e5fae4588911964f3
Roman R <r@resrom.com>
2022-05-30 16:08:41 +0800

    Swift Package Manager

---
+++ OnDocCalendar/Tests/OnDocCalendarTests/OnDocCalendarTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import OnDocCalendar
+
+final class OnDocCalendarTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(OnDocCalendar().text, "Hello, World!")
+    }
+}
