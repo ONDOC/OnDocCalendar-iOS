187141895157a616b830ed0ff230c123de3c89ce
Roman R <r@resrom.com>
2022-05-30 20:54:18 +0800

    Revert SPM

--- OnDocCalendar/Example/Pods/Target Support Files/OnDocCalendar/OnDocCalendar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OnDocCalendar : NSObject
-@end
-@implementation PodsDummy_OnDocCalendar
-@end
---
+++ README.md
@@ -0,0 +1,3 @@
+# OnDocCalendar
+
+A description of this package.
