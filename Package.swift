6b4bd9b5635bd41699977571b5d6e61478aff61c
Roman Resenchuk <r@resrom.com>
2022-06-08 14:52:34 +0800

    Revert "Added access to private SPM"
    This reverts commit cf544b8aeb38fbf770f3aa1b0aeace051fee657d.

--- Package.swift
+++ Package.swift
@@ -18,8 +18,6 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://romacv: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@github.com/ONDOC/OnDocCalendar-iOS.git",
-                   from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
